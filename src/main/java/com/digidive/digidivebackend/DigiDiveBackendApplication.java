c41a44bf5a3ea130ea04a724b91dca8ea608a449
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-09-29 03:13:52 +0300

    init

---
+++ src/main/java/com/digidive/digidivebackend/DigiDiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digidive.digidivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiDiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigiDiveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digidive/digidivebackend/DigiDiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digidive.digidivebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiDiveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
